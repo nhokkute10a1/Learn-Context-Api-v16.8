cdf0cc6d493fb99d1aae68aa2b53ec4928cd6317
Thu Thuỷ <doanthuy@Macintosh-HD-HighSierra-5.local>
2019-06-10 14:24:22 +0700

    add reducer

--- hooks-todo-app/src/App.js
+++ hooks-todo-app/src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
 import TodoApp from "./TodoApp";
 
+// function countReducer(state, action) {
+//   switch (action.type) {
+//     case 'INCEREMENT':
+//       return { count: state.count + action.amount };
+//     case 'DECEREMENT':
+//       return { count: state.count - action.amount };
+//     case 'RESET':
+//       return { count: 0 }
+//     default:
+
+//   }
+
+// }
+
 function App() {
-  return <TodoApp />;
+  // const [state, dispatch] = useReducer(countReducer, { count: 0 })
+  return (
+    <TodoApp/>
+    // <div>
+    //   <h1>{state.count}</h1>
+    //   <button onClick={() => dispatch({ type: 'INCEREMENT', amount: 1 })}> Add 1</button>
+    //   <button onClick={() => dispatch({ type: 'INCEREMENT', amount: 5 })}> Add 5</button>
+
+    //   <button onClick={() => dispatch({ type: 'DECEREMENT', amount: 1 })}>Subtract 1</button>
+    //   <button onClick={() => dispatch({ type: 'RESET'})}>RESET</button>
+
+    // </div>
+  );
 }
 
 export default App;
--- hooks-todo-app/src/Todo.js
+++ hooks-todo-app/src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import useToggleState from "./hooks/useToggleState";
 import EditTodoForm from "./EditTodoForm";
 import ListItem from "@material-ui/core/ListItem";
@@ -8,40 +8,42 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import { TodosContext } from "./context/todo.context";
 
-function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
+function Todo({ id, task, completed }) {
+  const { dispatch } = useContext(TodosContext);
+
   const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem style={{ height: "64px" }}>
       {isEditing ? (
         <EditTodoForm
-          editTodo={editTodo}
           id={id}
           task={task}
           toggleEditForm={toggle}
         />
       ) : (
-        <>
-          <Checkbox
-            tabIndex={-1}
-            checked={completed}
-            onClick={() => toggleTodo(id)}
-          />
-          <ListItemText
-            style={{ textDecoration: completed ? "line-through" : "none" }}
-          >
-            {task}
-          </ListItemText>
-          <ListItemSecondaryAction>
-            <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
-              <DeleteIcon />
-            </IconButton>
-            <IconButton aria-label='Edit' onClick={toggle}>
-              <EditIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </>
-      )}
+          <>
+            <Checkbox
+              tabIndex={-1}
+              checked={completed}
+              onClick={() => dispatch({ type: "TOGGLE", id: id })}
+            />
+            <ListItemText
+              style={{ textDecoration: completed ? "line-through" : "none" }}
+            >
+              {task}
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton aria-label='Delete' onClick={() => dispatch({ type: "REMOVE", id: id })}>
+                <DeleteIcon />
+              </IconButton>
+              <IconButton aria-label='Edit' onClick={toggle}>
+                <EditIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </>
+        )}
     </ListItem>
   );
 }
