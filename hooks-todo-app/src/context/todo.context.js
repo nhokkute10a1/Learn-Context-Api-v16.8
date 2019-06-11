9fd4455ce40d69e125eeff04cd1bd679a1862569
Thu Thuỷ <doanthuy@Macintosh-HD-HighSierra-5.local>
2019-06-11 11:32:46 +0700

    customer hook reducer and localstorage

--- hooks-todo-app/src/context/todo.context.js
+++ hooks-todo-app/src/context/todo.context.js
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from 'react'
-import todoReducer from '../reducers/todo.reducer'
+import React, { createContext } from 'react'
+import todoReducer from '../reducers/todo.reducer';
+import { useLocalStorageReducer } from '../hooks/useLocalStorageReducer';
 import useTodoState from '../hooks/useTodoState';
 
 const defaultTodos = [
@@ -11,7 +12,7 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-    const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+    const [todos, dispatch] = useLocalStorageReducer('todos', defaultTodos, todoReducer);
 
     return (
         <TodosContext.Provider value={todos}>
